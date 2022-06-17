2f39d41f774b3ec796e54d191babe2d5e04d1ef8
PANIKAN001 <95694958+PANKAN001@users.noreply.github.com>
2022-06-17 21:55:30 +0300

    Add

---
+++ MySuperPuperProject/NoExit.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySuperPuperProject
+{
+    /// <summary>
+    /// Логика взаимодействия для NoExit.xaml
+    /// </summary>
+    public partial class NoExit : Page
+    {
+        public NoExit()
+        {
+            InitializeComponent();
+        }
+
+        private void ButPokaz_Click(object sender, RoutedEventArgs e)
+        {
+            Password2.Text = Password.Password.ToString();
+            ButSkryt.Visibility = Visibility.Visible; ButPokaz.Visibility = Visibility.Hidden;
+            Password2.Visibility = Visibility.Visible; Password.Visibility = Visibility.Hidden;
+        }
+
+        private void ButSkryt_Click(object sender, RoutedEventArgs e)
+        {
+            ButSkryt.Visibility = Visibility.Hidden; ButPokaz.Visibility = Visibility.Visible;
+            Password2.Visibility = Visibility.Hidden; Password.Visibility = Visibility.Visible;
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Register());
+        }
+
+        private void Brn_Vhod_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Prodavec());
+        }
+    }
+}
